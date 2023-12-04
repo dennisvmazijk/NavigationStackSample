5e5c531c03572198e57f07501793f3d812a99786
Dennis van Mazijk <144079059+dennisvmazijk@users.noreply.github.com>
2023-12-04 15:18:53 +0900

    Initial Commit

---
+++ NavigationStackSample/NavigationStackSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackSampleApp.swift
+//  NavigationStackSample
+//
+//  Created by Dennis van Mazijk on 2023/12/04.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
